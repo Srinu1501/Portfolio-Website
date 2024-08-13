0d41a9dc6e6325c7118c9adab0ca8c086736cea6
Srinivasa Sai <saisrivatsasa@gmail.com>
2024-08-13 21:10:13 +0530

    First Commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var typed = new typed(".text", {
-  Strings: ["FrontEnd Developer", "Web Developer", "Java Developer"],
+var typed = new Typed(".typed", {
+  strings: ["FrontEnd Developer", "Web Developer", "Java Developer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
